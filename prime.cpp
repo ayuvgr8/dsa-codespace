98696b5f632fcc450413dd68b7ef2286bdba24ed
Ayush Verma <49532650+ayuvgr8@users.noreply.github.com>
2023-01-29 15:55:23 +0000

    PrimeNumber

--- prime.cpp
+++ prime.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
 int main (){
-    cout<< " Hi Pluto!"<< endl;
+
+    int n ;
+    cin>>n;
+    int i=2;
+    while (i<n){
+        //divide ho gya toh prime nahi hoga
+        if(i%n==0){
+            cout<<" Not Prime for :"<<i<<endl;
+        }else{
+            cout<<"Prime for :"<<i<<endl;
+                }
+        i=i+1;
+    }
 }
